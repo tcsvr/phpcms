382c0e545e94aa5a3976a37f6f67d87664ce78cb
tcsvr <1484552626@qq.com>
2019-07-06 19:40:29 +0800

    init

---
+++ caches/caches_tpl_data/caches_data/e2c10f97abe586e8c28ac40d3e2202b7.cache.php
@@ -0,0 +1,15 @@
+<?php
+return array (
+  '6-1' => 
+  array (
+    'title' => '将AI 接入VR',
+    'description' => 'ObEN成立于 2014 年，成立之初曾募集到 15 万美元的种子轮融资，入围过 HTC Vive X 孵化器。2016年11月曾获得770万美元A论融资，领投方是中国盾安集团旗下投资分部 CrestValue Capital，参投方包括知名',
+    'thumb' => 'http://www.phpcms.com/uploadfile/2019/0704/20190704094344174.jpg',
+    'inputtime' => '1562247551',
+    'url' => 'http://www.phpcms.com/index.php?m=content&c=index&a=show&catid=6&id=1',
+    'id' => '1',
+    'catid' => '6',
+    'listorder' => '1',
+  ),
+);
+?>
